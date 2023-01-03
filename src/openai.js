e68dcd440a1321faee433c6641b9cec58203e7ba
Patrick Girnghuber <p.girnghuber@yahoo.de>
2023-01-03 21:19:48 +0100

    added open ai opinion for songs

---
+++ src/openai.js
@@ -0,0 +1,49 @@
+require("dotenv").config();
+
+const request = require("request");
+
+async function getOpinionForSong(artist, song) {
+  const prompt = `Greet the fan of ${artist} and then tell them something about their favourite song: '${song}'. Call the fan by the typical name of a fan of ${artist}.`;
+
+  const chatgptRequestData = {
+    url: `https://api.openai.com/v1/completions`,
+    headers: {
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: prompt,
+      max_tokens: 50,
+      model: "text-davinci-003",
+      temperature: 0.5,
+    }),
+  };
+
+  return new Promise((resolve, reject) => {
+    request.post(chatgptRequestData, (err, res, body) => {
+      const response = JSON.parse(body);
+      console.log(response.choices[0].text);
+      resolve(response.choices[0].text);
+    });
+  });
+}
+
+async function listModels() {
+  const chatgptRequestData = {
+    url: `https://api.openai.com/v1/models`,
+    headers: {
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+      "Content-Type": "application/json",
+    },
+  };
+
+  return new Promise((resolve, reject) => {
+    request.get(chatgptRequestData, (err, res, body) => {
+      const response = JSON.parse(body);
+      console.log(response);
+      resolve(response);
+    });
+  });
+}
+
+module.exports = { listModels, getOpinionForSong };
